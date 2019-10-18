7444857147d05816ef6d6be7d823bc7cb3e73646
mim-3pleE <mim3plee@gmail.com>
2019-10-18 16:17:16 +0100

    intent to connect sport quiz

--- app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/MainActivity.java
+++ app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.josycom.aad_team_44_animation_challenge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem sports = menu.add(0, 1, 0, "Sport Quiz");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case 1:
+                startActivity(new Intent(this, SportQuizActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
