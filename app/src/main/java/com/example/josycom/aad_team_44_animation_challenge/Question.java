568edabde727bf0b14fc446f594e17517fc37ba0
Abdullahi Usman <abdullahismn7@gmail.com>
2019-10-20 13:16:12 +0100

    add: Question Activity

---
+++ app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/Question.java
@@ -0,0 +1,25 @@
+package com.example.josycom.aad_team_44_animation_challenge;
+
+import androidx.room.Dao;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverters;
+
+import java.util.ArrayList;
+
+
+@Entity
+public class Question {
+
+    @Ignore
+    private final int NUMBER_OF_ANSWERS = 4;
+    @PrimaryKey
+    int primaryKey;
+
+    String Question;
+    @TypeConverters()
+    ArrayList<String> answers = new ArrayList<>(NUMBER_OF_ANSWERS);
+    int answer;
+
+}
