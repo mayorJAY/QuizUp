ac4fe8e925e163098a84ab1d0ad83d8cf0e9eca8
Abdullahi Usman <abdullahismn7@gmail.com>
2019-10-21 10:10:29 +0100

    feat: implement loadQuestion()

--- app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/DataManager.java
+++ app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/DataManager.java
@@ -1,10 +1,14 @@
 package com.example.josycom.aad_team_44_animation_challenge;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.ContactsContract;
 
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
+import java.util.List;
 
 public class DataManager {
 
@@ -26,7 +30,27 @@ public class DataManager {
         return dataManager;
     }
 
-    public void loadQuestion(QuizListManager.Quiz quiz){
-        database.getQuestionDao().getAll("");
+
+    interface OnQuestionsLoadedListener{
+        void onQuestionsLoaded(List<Question> questions);
+    }
+    public void loadQuestion(final QuizListManager.Quiz quiz, final OnQuestionsLoadedListener listener){
+
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                listener.onQuestionsLoaded((List<Question>)msg.obj);
+            }
+        };
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Message message = Message.obtain();
+
+                message.obj = database.getQuestionDao().getAll(quiz.getName());
+                handler.sendMessage(message);
+            }
+        });
     }
 }
