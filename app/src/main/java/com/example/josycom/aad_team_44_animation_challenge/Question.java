e1436e29720ec2dbcf5ca5f8e43b63e42fb9b909
Abdullahi Usman <abdullahismn7@gmail.com>
2019-10-21 10:06:33 +0100

    fix: update Question

--- app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/Question.java
+++ app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/Question.java
@@ -1,12 +1,14 @@
 package com.example.josycom.aad_team_44_animation_challenge;
 
-import androidx.room.Dao;
 import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Entity
@@ -18,8 +20,31 @@ public class Question {
     int primaryKey;
 
     String Question;
-    @Ignore
-    ArrayList<String> answers = new ArrayList<>(NUMBER_OF_ANSWERS);
+
+    String category;
+
+    @TypeConverters(AnswerConverter.class)
+    List<String> answers = new ArrayList<>();
     int answer;
 
+    static class AnswerConverter {
+
+        @TypeConverter
+        public String answersFromArray(List<String> answers) {
+            String a = answers.get(0);
+
+
+            for (int i = 1; i < answers.size(); i++) {
+                a = a.concat(":").concat(answers.get(i));
+            }
+
+            return a;
+        }
+
+        @TypeConverter
+        public List<String> answersToArray(String answers) {
+            return Arrays.asList(answers.split(":"));
+        }
+
+    }
 }
