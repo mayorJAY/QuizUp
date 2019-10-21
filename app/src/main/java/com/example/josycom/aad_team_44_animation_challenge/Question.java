fd575f2b5e77e5f7c8335e6542b19729366be0fc
Abdullahi Usman <abdullahismn7@gmail.com>
2019-10-21 11:12:30 +0100

    feat: add indexing

--- app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/Question.java
+++ app/src/main/java/com/example/josycom/aad_team_44_animation_challenge/Question.java
@@ -2,6 +2,7 @@ package com.example.josycom.aad_team_44_animation_challenge;
 
 import androidx.room.Entity;
 import androidx.room.Ignore;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
@@ -11,21 +12,29 @@ import java.util.Arrays;
 import java.util.List;
 
 
-@Entity
+@Entity(indices = {@Index(unique = false, value = "Category")})
 public class Question {
 
-    @Ignore
-    private final int NUMBER_OF_ANSWERS = 4;
+    public Question(){}
+
+    public Question(String category, String question, int answer, String... options){
+        this.Question = question;
+        this.Category = category;
+        this.answer = answer;
+        this.options.addAll(Arrays.asList(options));
+    }
+
     @PrimaryKey
     int primaryKey;
 
+    String Category;
+
     String Question;
 
-    String category;
+    int answer;
 
     @TypeConverters(AnswerConverter.class)
-    List<String> answers = new ArrayList<>();
-    int answer;
+    List<String> options = new ArrayList<>();
 
     static class AnswerConverter {
 
